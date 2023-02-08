560166e9597cb0e523507e89f5d748e0e77e8060
Viktor <mail.victor.kostov@gmail.com>
2023-02-08 15:28:40 +0200

    test

--- .github/foo.sh
+++ .github/foo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cat "${REPO_DIR}/go.mod"
+LC_ALL=C sed -E -n 's/.*k8s\.io\/client-go => k8s\.io\/client-go (.+)/\1/p' go.mod 
 #gardener_ver="$(sed -E -n 's/\s*github.com\/gardener\/gardener\s+(\S+)/\1/p' "${REPO_DIR}/go.mod" )"
 #echo "$gardener_ver"
 #text="$(curl  "https://api.github.com/repos/gardener/gardener/contents/go.mod?ref=v1.60.0" --header "authorization: Bearer ${GH_TOKEN}" )"
