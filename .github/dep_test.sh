02800f0d7361a8f1d143ef2517e371d1f2c51b9c
Viktor <mail.victor.kostov@gmail.com>
2023-02-08 09:29:06 +0200

    debug print

--- .github/dep_test.sh
+++ .github/dep_test.sh
@@ -3,7 +3,9 @@
 go mod edit -require "github.com/gardener/gardener@v1.6.0"
 
 gardener_ver="$(sed -E -n 's/\s*github.com\/gardener\/gardener\s+(\S+)/\1/p' go.mod)"
-gardener_replace="$(curl  "https://api.github.com/repos/gardener/gardener/contents/go.mod?ref=${gardener_ver}" | jq -r ".content" | base64 --decode | sed -E -n 's/.*k8s\.io\/client-go => k8s\.io\/client-go (.+)/\1/p')"
+text="$(curl  "https://api.github.com/repos/gardener/gardener/contents/go.mod?ref=${gardener_ver}" | jq -r ".content" | base64 --decode )"
+echo "$text"
+gardener_replace="$(echo "$text"| sed -E -n 's/.*k8s\.io\/client-go => k8s\.io\/client-go (.+)/\1/p')"
 go mod edit -replace "k8s.io/client-go=k8s.io/client-go@${gardener_replace}"
 
 go mod edit -require "github.com/gardener/gardener@v1.60.0"
