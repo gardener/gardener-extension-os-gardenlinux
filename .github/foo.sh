436277bdc983c674e4d587b8eebca1c27745ad6a
Viktor <mail.victor.kostov@gmail.com>
2023-02-08 10:28:35 +0200

    test action token

--- .github/foo.sh
+++ .github/foo.sh
@@ -4,5 +4,5 @@ set -e
 
 gardener_ver="$(sed -E -n 's/\s*github.com\/gardener\/gardener\s+(\S+)/\1/p' go.mod)"
 echo "$gardener_ver"
-text="$(curl  "https://api.github.com/repos/gardener/gardener/contents/go.mod?ref=v1.60.0" )"
+text="$(curl  "https://api.github.com/repos/gardener/gardener/contents/go.mod?ref=v1.60.0" --header "authorization: Bearer ${GH_TOKEN}" )"
 echo "$text"
