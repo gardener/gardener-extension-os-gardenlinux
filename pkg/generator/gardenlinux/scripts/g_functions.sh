19b2338834a735b44aee56e46c25b568826895a6
Thomas Buchner <thomas.buchner@sap.com>
2023-03-15 16:07:25 +0100

    improve running container detection

--- pkg/generator/gardenlinux/scripts/g_functions.sh
+++ pkg/generator/gardenlinux/scripts/g_functions.sh
@@ -41,13 +41,23 @@ function check_current_cgroup {
 }
 
 function check_running_containerd_tasks {
-    # determine if containerd has running containers in the k8s.io namespace
+    containerd_runtime_status_dir=/run/containerd/io.containerd.runtime.v2.task/k8s.io
 
-    running_containers="$(ctr -n k8s.io task list | tail -n +2)"
-
-    if [ -z "$running_containers" ]; then
+    # if the status dir for k8s.io namespace does not exist, there are no containers
+    # in said namespace
+    if [ ! -d $containerd_runtime_status_dir ]; then
+        echo "$containerd_runtime_status_dir does not exists - no tasks in k8s.io namespace" 
         return 0
     fi
 
+    # count the number of containerd tasks in the k8s.io namespace
+    num_tasks=$(ls -1 /run/containerd/io.containerd.runtime.v2.task/k8s.io/ | wc -l)
+
+    if [ "$num_tasks" -eq 0 ]; then
+        echo "no active tasks in k8s.io namespace" 
+        return 0
+    fi
+
+    echo "there are $num_tasks active tasks in the k8s.io containerd namespace"
     return 1
 }
