ea76ed77b158d6e5f2751dd7fcbb6e58f6b64953
Tim Usner <tim.usner@sap.com>
2024-05-16 18:18:17 +0200

    Remove vendor dir

--- vendor/github.com/gardener/gardener/pkg/apis/seedmanagement/doc.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-
-// +k8s:deepcopy-gen=package
-
-// Package seedmanagement is the internal version of the API.
-// +groupName=seedmanagement.gardener.cloud
-package seedmanagement
--- vendor/github.com/gardener/gardener/pkg/extensions/constants.go
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-
-package extensions
-
-// HeartBeatResourceName is the name of the lease that is managed by the extensions heartbeat controller.
-const HeartBeatResourceName = "gardener-extension-heartbeat"
